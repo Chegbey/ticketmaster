ea5b56166014a507156010437e56d35e63077c04
ChegBey <mrnarcisik@gmail.com>
2016-11-22 16:20:40 +0100

    Send email to user

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'ticketmaster.notif@gmail.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'ticketmaster.notif@gmail.com',
+    :password => 'Ticketmaster1',
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
